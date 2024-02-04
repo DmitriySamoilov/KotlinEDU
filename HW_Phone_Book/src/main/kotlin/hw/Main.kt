1de637313ad34a66710be6ca6a5366a1c3f6584d
Dmitriy Samoilov <ddd2008ok@gmail.com>
2024-02-05 02:23:59 +0300

    hw4

--- HW_Phone_Book/src/main/kotlin/hw/Main.kt
+++ HW_Phone_Book/src/main/kotlin/hw/Main.kt
@@ -12,37 +12,16 @@ fun main() {
 
 private fun readCommand(): Command {
     var commandLine = readln().split(" ")
-    var resultCommand: Command
     when (commandLine[0].lowercase()) {
-        "export" -> {
-            if (commandLine.size == 2){
-                resultCommand = Command.Export(commandLine[1])
-                return resultCommand }
-        }
+        "export" -> if (commandLine.size == 2) return Command.Export(commandLine[1])
         "exit" -> return Command.Exit
-        "show" -> {
-            if (commandLine.size > 1){
-                resultCommand = Command.Show(name = commandLine[1])
-                return resultCommand }
-        }
-        "find" -> {
-            if (commandLine.size > 1){
-            resultCommand = Command.Find(whatToFind = commandLine[1])
-                return resultCommand }
-        }
+        "show" -> if (commandLine.size == 2) return Command.Show(name = commandLine[1])
+        "find" -> if (commandLine.size == 2) return Command.Find(whatToFind = commandLine[1])
         "add" -> {
             if (commandLine.size == 4) {
-
                 when (commandLine[2].lowercase()) {
-                    "phone" -> {
-                        resultCommand = Command.AddPhone(commandLine[1], commandLine[3])
-                            return resultCommand
-                    }
-
-                    "email" -> {
-                        resultCommand = Command.AddEmail(commandLine[1], commandLine[3])
-                            return resultCommand
-                    }
+                    "phone" -> return Command.AddPhone(commandLine[1], commandLine[3])
+                    "email" -> return Command.AddEmail(commandLine[1], commandLine[3])
                 }
             }
         }
